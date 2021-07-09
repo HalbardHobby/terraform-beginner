2eeafe6a74d4faf299513e58a295d467df585c85
Halbard <HalbardHobby@users.noreply.github.com>
2021-07-09 11:37:07 -0500

    🕸 Added subnets

--- aws_deployment/main.tf
+++ aws_deployment/main.tf
@@ -21,6 +21,25 @@ resource "aws_vpc" "vpc_main" {
   }
 }
 
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.vpc_main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "TF_private_subnet"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.vpc_main.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "TF_public_subnet"
+  }
+}
+
 module "ec2" {
   source = "./ec2"
 }
